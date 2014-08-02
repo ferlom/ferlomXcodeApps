2ab90948310e85ce9bea140f47a95ee987725fd3
Ferlom Lombos <ferlom2000@gmail.com>
2014-08-02 17:35:04 +0200

    Initial Commit

---
+++ PruebaDeConsola/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PruebaDeConsola
+//
+//  Created by FERNANDO LOMBOS on 02/08/14.
+//  Copyright (c) 2014 ferlom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
